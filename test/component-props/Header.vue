7218383e815a4fa397a660f21c57fa31db6bc517
h-hashimoto0108 <h.hashimoto.web@gmail.com>
2019-11-25 13:33:16 +0900

    add compornent test

---
+++ test/component-props/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      title:"header",
+      text:"Hello wings"
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+div{
+  border: 1px solid blue
+}
+
+h1{
+  color:blue
+}
+
+p{
+  color:blue
+}
+
+</style>
